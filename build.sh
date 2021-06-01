3fc6c889562a18a8a3c8749afc2a08e9da422750
nrichard <nils.richard@etudiant.univ-rennes1.fr>
2021-06-01 21:38:04 +0200

    🔨 Fixed linux dev-build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-rm -f build/*
-cp assets/icon.png build/icon.png
-cp manifest.json build/manifest.json
-cp src/*.html build/
+rm -f ./build/*
+cp ./assets/icon.png ./build/icon.png
+cp ./manifest.json ./build/manifest.json
+cp ./src/*.html ./build/
 npx tsc
